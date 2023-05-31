3bb0738595054da9d234b0d194bf13fe07db4b2d
jezrreloriginal <124934158+jezrreloriginal@users.noreply.github.com>
2023-05-31 16:22:19 -0300

    Add files via upload

---
+++ main (3).kt
@@ -0,0 +1,16 @@
+
+fun main (){
+  println( "¿cual es tu edad ? ? ")
+  var idade = readLine()!!
+  println("sua idade é :" +idade)
+
+  val num = idade.toInt()
+
+if (num <= 17){
+   println("eres menor de edad")
+}else{
+  println("eres mayor de edad")
+  
+}
+ } 
+       
